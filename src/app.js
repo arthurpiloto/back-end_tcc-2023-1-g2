6226846c8128472f75583bf1b51d5cf6fc1d7062
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2023-03-09 09:58:53 -0300

    adding permisions

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const app = express()
 app.use((request, response, next) => {
     response.header(`Access-Control-Allow-Origin`, `*`)
     response.header(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, OPTIONS`)
+    response.header('Access-Control-Allow-Headers', 'Content-type')
 
     app.use(cors())
     next()
