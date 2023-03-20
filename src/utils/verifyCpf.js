854b62e2e6f61e56ff13f4d898f134e4e56cc5f0
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2023-03-20 08:21:31 -0300

    verify length

--- src/utils/verifyCpf.js
+++ src/utils/verifyCpf.js
@@ -5,47 +5,51 @@ DATA DE CRIAÇÃO: 16/03/2023
 VERSÃO: 1.0
 ************************************************************************/
 const verifyCpf = async (cpf) => {
-    // SEPARA TODOS OS CARACTERES DO CPF
-    var chars = cpf.split('')
+    if (cpf.length == 11) {
+        // SEPARA TODOS OS CARACTERES DO CPF
+        var chars = cpf.split('')
 
-    // SALVA OS 9 PRIMEIROS DIGITOS
-    let numberOne = chars[0]
-    let numberTwo = chars[1]
-    let numberThree = chars[2]
-    let numberFour = chars[3]
-    let numberFive = chars[4]
-    let numberSix = chars[5]
-    let numberSeven = chars[6]
-    let numberEight = chars[7]
-    let numberNine = chars[8]
+        // SALVA OS 9 PRIMEIROS DIGITOS
+        let numberOne = chars[0]
+        let numberTwo = chars[1]
+        let numberThree = chars[2]
+        let numberFour = chars[3]
+        let numberFive = chars[4]
+        let numberSix = chars[5]
+        let numberSeven = chars[6]
+        let numberEight = chars[7]
+        let numberNine = chars[8]
 
-    // SOMA TODOS OS VALORES, MULTIPLICANDO-OS POR NÚMEROS DECRESCENTES A PARTIR DO 10
-    let sumNumbers = (numberOne * 10) + (numberTwo * 9) + (numberThree * 8) + (numberFour * 7) + (numberFive * 6) + (numberSix * 5) + (numberSeven * 4) + (numberEight * 3) + (numberNine * 2)
+        // SOMA TODOS OS VALORES, MULTIPLICANDO-OS POR NÚMEROS DECRESCENTES A PARTIR DO 10
+        let sumNumbers = (numberOne * 10) + (numberTwo * 9) + (numberThree * 8) + (numberFour * 7) + (numberFive * 6) + (numberSix * 5) + (numberSeven * 4) + (numberEight * 3) + (numberNine * 2)
 
-    // DIVIDIMOS A SOMA TOTAL POR 11
-    let firstDigit = sumNumbers % 11
-    if (firstDigit < 2) {
-        firstDigit = 0
-    }
-    if (firstDigit > 2) {
-        // SUBTRAI O RESTO DA DIVISÃO POR 11
-        firstDigit = 11 - firstDigit
-    }
+        // DIVIDIMOS A SOMA TOTAL POR 11
+        let firstDigit = sumNumbers % 11
+        if (firstDigit < 2) {
+            firstDigit = 0
+        }
+        if (firstDigit > 2) {
+            // SUBTRAI O RESTO DA DIVISÃO POR 11
+            firstDigit = 11 - firstDigit
+        }
 
-    // SOMA TODOS OS VALORES, MULTIPLICANDO-OS POR NÚMEROS DECRESCENTES A PARTIR DO 11 POR CONTA DO PRIMEIRO DIGITO
-    let sumNumbersPt2 = (numberOne * 11) + (numberTwo * 10) + (numberThree * 9) + (numberFour * 8) + (numberFive * 7) + (numberSix * 6) + (numberSeven * 5) + (numberEight * 4) + (numberNine * 3) + (firstDigit * 2)
+        // SOMA TODOS OS VALORES, MULTIPLICANDO-OS POR NÚMEROS DECRESCENTES A PARTIR DO 11 POR CONTA DO PRIMEIRO DIGITO
+        let sumNumbersPt2 = (numberOne * 11) + (numberTwo * 10) + (numberThree * 9) + (numberFour * 8) + (numberFive * 7) + (numberSix * 6) + (numberSeven * 5) + (numberEight * 4) + (numberNine * 3) + (firstDigit * 2)
 
-    let secondDigit = sumNumbersPt2 % 11
-    if (secondDigit < 2) {
-        secondDigit = 0
-    }
-    if (secondDigit > 2) {
-        // SUBTRAI O RESTO DA DIVISÃO POR 11
-        secondDigit = 11 - secondDigit
-    }
+        let secondDigit = sumNumbersPt2 % 11
+        if (secondDigit < 2) {
+            secondDigit = 0
+        }
+        if (secondDigit > 2) {
+            // SUBTRAI O RESTO DA DIVISÃO POR 11
+            secondDigit = 11 - secondDigit
+        }
 
-    if (chars[9] == firstDigit && chars[10] == secondDigit) {
-        return true
+        if (chars[9] == firstDigit && chars[10] == secondDigit) {
+            return true
+        } else {
+            return false
+        }
     } else {
         return false
     }
--- src/utils/verifyRg.js
+++ src/utils/verifyRg.js
@@ -5,34 +5,38 @@ DATA DE CRIAÇÃO: 17/03/2023
 VERSÃO: 1.0
 ************************************************************************/
 const verifyRg = async (rg) => {
-    // SEPARA TODOS OS CARACTERES DO RG
-    var chars = rg.toUpperCase().split('')
+    if (rg.length == 9) {
+        // SEPARA TODOS OS CARACTERES DO RG
+        var chars = rg.toUpperCase().split('')
 
-    // SALVA OS 9 PRIMEIROS DIGITOS
-    let numberOne = chars[0]
-    let numberTwo = chars[1]
-    let numberThree = chars[2]
-    let numberFour = chars[3]
-    let numberFive = chars[4]
-    let numberSix = chars[5]
-    let numberSeven = chars[6]
-    let numberEight = chars[7]
+        // SALVA OS 9 PRIMEIROS DIGITOS
+        let numberOne = chars[0]
+        let numberTwo = chars[1]
+        let numberThree = chars[2]
+        let numberFour = chars[3]
+        let numberFive = chars[4]
+        let numberSix = chars[5]
+        let numberSeven = chars[6]
+        let numberEight = chars[7]
 
-    // // SOMA TODOS OS VALORES, MULTIPLICANDO-OS POR NÚMEROS DECRESCENTES A PARTIR DO 10
-    let sumNumbers = (numberOne * 2) + (numberTwo * 3) + (numberThree * 4) + (numberFour * 5) + (numberFive * 6) + (numberSix * 7) + (numberSeven * 8) + (numberEight * 9)
+        // // SOMA TODOS OS VALORES, MULTIPLICANDO-OS POR NÚMEROS DECRESCENTES A PARTIR DO 10
+        let sumNumbers = (numberOne * 2) + (numberTwo * 3) + (numberThree * 4) + (numberFour * 5) + (numberFive * 6) + (numberSix * 7) + (numberSeven * 8) + (numberEight * 9)
 
-    // // DIVIDIMOS A SOMA TOTAL POR 11
-    let lastDigit = 11 - (sumNumbers % 11)
-    if (lastDigit == 10) {
-        lastDigit = "X"
-    }
-    if (lastDigit == 11) {
-        lastDigit = "0"
-    }
+        // // DIVIDIMOS A SOMA TOTAL POR 11
+        let lastDigit = 11 - (sumNumbers % 11)
+        if (lastDigit == 10) {
+            lastDigit = "X"
+        }
+        if (lastDigit == 11) {
+            lastDigit = "0"
+        }
 
 
-    if (chars[8] == lastDigit) {
-        return true
+        if (chars[8] == lastDigit) {
+            return true
+        } else {
+            return false
+        }
     } else {
         return false
     }
