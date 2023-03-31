e1085d6e0bf5f7e2cbc24332ea88d2f63a218b0f
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2023-03-31 11:05:47 -0300

    working with the inner join select

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -9,6 +9,7 @@ VERSÃO: 1.0
 const { insertDriver, updateDriver, deleteDriver, selectAllDrivers, selectDriverIdByCPF, selectDriverById } = require('../models/DAO/driver.js')
 const { verifyCpf } = require('../utils/verifyCpf.js')
 const { verifyRg } = require('../utils/verifyRg.js')
+const { createDriverJson } = require('../utils/createDriverJson.js')
 const { MESSAGE_ERROR, MESSAGE_SUCCESS } = require('../modules/config.js')
 
 const novoDriver = async (driver) => {
@@ -119,9 +120,8 @@ const listarDriverById = async (id) => {
         const result = await selectDriverById(id)
 
         if (result) {
-            let driverJson = {}
-            driverJson.driver = result
-            return { status: 200, message: driverJson }
+            let jsonData = await createDriverJson(result)
+            return { status: 200, message: jsonData }
         } else {
             return { status: 404, message: MESSAGE_ERROR.NOT_FOUND_DB }
         }
---
+++ src/utils/createDriverJson.js
@@ -0,0 +1,50 @@
+/************************************************************************
+OBJETIVO: ARQUIVO RESPONSÁVEL PELA CRIAÇÃO DO JSON DE MOTORISTA
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 16/03/2023
+VERSÃO: 1.0
+************************************************************************/
+const createDriverJson = async (result) => {
+    let jsonData = {}
+    let driverJson = {}
+    let vanJson = {}
+    let modelJson = {}
+    result.forEach(element => {
+        modelJson = {
+            id: element.id_modelo,
+            modelo: element.modelo_van,
+            status: element.status_modelo
+        }
+        vanJson = {
+            id: element.id_van,
+            placa: element.placa,
+            foto: element.foto_van,
+            quantidade_vagas: element.quantidade_vagas,
+            status: element.status_van,
+            modelo: modelJson
+        }
+        driverJson = {
+            id: element.id_motorista,
+            nome: element.nome,
+            email: element.email,
+            rg: element.rg,
+            cpf: element.cpf,
+            cnh: element.cnh,
+            telefone: element.telefone,
+            data_nascimento: element.data_nascimento,
+            inicio_carreira: element.inicio_carreira,
+            senha: element.senha,
+            foto: element.foto_motorista,
+            avaliacao: element.avaliacao,
+            descricao: element.descricao,
+            status: element.status_motorista,
+            vans: [vanJson]
+        }
+    })
+    jsonData.driver = driverJson
+    return jsonData
+}
+
+module.exports = {
+    createDriverJson
+}
