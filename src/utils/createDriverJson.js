17166a3706691b09e4d362d554f78d7ec00dca20
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2023-03-31 11:25:56 -0300

    upload file

--- src/utils/createDriverJson.js
+++ src/utils/createDriverJson.js
@@ -5,44 +5,48 @@ DATA DE CRIAÇÃO: 16/03/2023
 VERSÃO: 1.0
 ************************************************************************/
 const createDriverJson = async (result) => {
-    let jsonData = {}
-    let driverJson = {}
-    let vanJson = {}
-    let modelJson = {}
-    result.forEach(element => {
-        modelJson = {
-            id: element.id_modelo,
-            modelo: element.modelo_van,
-            status: element.status_modelo
-        }
-        vanJson = {
-            id: element.id_van,
-            placa: element.placa,
-            foto: element.foto_van,
-            quantidade_vagas: element.quantidade_vagas,
-            status: element.status_van,
-            modelo: modelJson
-        }
-        driverJson = {
-            id: element.id_motorista,
-            nome: element.nome,
-            email: element.email,
-            rg: element.rg,
-            cpf: element.cpf,
-            cnh: element.cnh,
-            telefone: element.telefone,
-            data_nascimento: element.data_nascimento,
-            inicio_carreira: element.inicio_carreira,
-            senha: element.senha,
-            foto: element.foto_motorista,
-            avaliacao: element.avaliacao,
-            descricao: element.descricao,
-            status: element.status_motorista,
-            vans: [vanJson]
-        }
-    })
-    jsonData.driver = driverJson
-    return jsonData
+    let arrayDrivers = []
+    for (let index = 0; index <= result.length; index++) {
+        let jsonData = {}
+        let driverJson = {}
+        let vanJson = {}
+        let modelJson = {}
+        result.forEach(element => {
+            modelJson = {
+                id: element.id_modelo,
+                modelo: element.modelo_van,
+                status: element.status_modelo
+            }
+            vanJson = {
+                id: element.id_van,
+                placa: element.placa,
+                foto: element.foto_van,
+                quantidade_vagas: element.quantidade_vagas,
+                status: element.status_van,
+                modelo: modelJson
+            }
+            driverJson = {
+                id: element.id_motorista,
+                nome: element.nome,
+                email: element.email,
+                rg: element.rg,
+                cpf: element.cpf,
+                cnh: element.cnh,
+                telefone: element.telefone,
+                data_nascimento: element.data_nascimento,
+                inicio_carreira: element.inicio_carreira,
+                senha: element.senha,
+                foto: element.foto_motorista,
+                avaliacao: element.avaliacao,
+                descricao: element.descricao,
+                status: element.status_motorista,
+                vans: [vanJson]
+            }
+        })
+        jsonData.driver = driverJson
+        arrayDrivers.push(jsonData)
+    }
+    console.log(arrayDrivers)
 }
 
 module.exports = {
