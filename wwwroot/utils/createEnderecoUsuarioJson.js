b5b70c053d619aecb39fa81d49271ccdbd0be7f7
Arthur Piloto <arthurpiloto.github@gmail.com>
2023-05-09 08:28:06 -0300

    error fixed

--- wwwroot/utils/createEnderecoUsuarioJson.js
+++ wwwroot/utils/createEnderecoUsuarioJson.js
@@ -5,13 +5,13 @@ DATA DE CRIAÇÃO: 07/05/2023
 VERSÃO: 1.0
 ************************************************************************/
 const { listarEnderecoById } = require('../controllers/enderecoController.js')
-const { listarUserById } = require('../controllers/userController.js')
+const userController = require('../controllers/userController.js')
 
 const createEnderecoUsuarioJson = async (enderecoUsuario, message) => {
     let messageJson = {}
 
     messageJson.id = enderecoUsuario.id
-    messageJson.usuario = await listarUserById(enderecoUsuario.id_usuario)
+    messageJson.usuario = await userController.listarUserById(enderecoUsuario.id_usuario)
     messageJson.endereco = await listarEnderecoById(enderecoUsuario.id_endereco)
 
     return messageJson
